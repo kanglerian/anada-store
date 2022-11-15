8a7a24bd7f6969a4f6dc789362c95b62314e12b8
kanglerian <kanglerian@gmail.com>
2022-11-15 11:20:18 +0700

    new price

--- src/components/Kaos.js
+++ src/components/Kaos.js
@@ -16,9 +16,11 @@ function Kaos(props) {
             <div className="py-3">
                 <h5 className="bg-slate-200 w-24 rounded-lg mx-auto font-medium">{props.title.code}</h5>
                 <h5 className="text-md font-bold text-slate-700 mt-1">{props.title.name}</h5>
-                <h5 className="text-md line-through text-slate-600 my-1">Rp100.000</h5>
-                <div className="bg-red-500 w-32 py-1 mx-auto rounded-lg text-white">
-                    <h5 className="font-bold text-xl">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 4 }).format(props.title.price)}</h5>
+                {props.title.diskon > 0 && 
+                    <h5 className="text-md line-through text-slate-600 my-1">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 4 }).format(props.title.price)}</h5>
+                }
+                <div className="bg-red-500 w-32 py-1 mt-1 mx-auto rounded-lg text-white">
+                    <h5 className="font-bold text-xl">{new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 4 }).format(props.title.diskon > 0 ? props.title.diskon : props.title.price)}</h5>
                 </div>
             </div>
         </div>
